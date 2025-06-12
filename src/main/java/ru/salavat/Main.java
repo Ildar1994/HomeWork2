2af934b3c7d9ac819b8821fb60c513b59687697c
Ildar1994 <86912735+Ildar1994@users.noreply.github.com>
2025-06-12 16:37:59 +0300

    Update Main.java

--- src/main/java/ru/salavat/Main.java
+++ src/main/java/ru/salavat/Main.java
@@ -2,7 +2,7 @@ package ru.salavat;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Main {3333
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
